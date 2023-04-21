554228d89211e143a6da37f58449510c68703586
Manis7733 <69597974+Manis7733@users.noreply.github.com>
2023-04-21 12:40:31 +0530

    before day3 (in between day 3)

---
+++ GoogleForms/forms/utils.py
@@ -0,0 +1,25 @@
+import pymongo
+from django.utils import timezone 
+
+from bson import ObjectId
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["GoogleForms"]
+col = db["GoogleForms"]
+
+class Form:
+    def create(self):
+        obj = col.insert_one({
+            "created":timezone.now()
+        })  
+        return str(obj.inserted_id)
+
+    def find(self,pk):
+        query = {
+            "_id":ObjectId(pk)
+        }
+        print(col.find(query))
+        print(type(col.find(query)))
+        obj = col.find(query).next() #cursor object
+
+        return obj
---
+++ GoogleForms/forms/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.http import Http404
+from .utils import Form
+    
+
+
+# Create your views here.
+form = Form()
+
+def get_random_uuid():
+    return "my_simple_uuid"
+
+def home(request):
+    return render(request, "home.html")
+
+def create(request):
+    pk = form.create()
+    return HttpResponseRedirect(reverse_lazy("forms:update_form",args=[pk]))
+
+def update_form(request,pk):
+    try:
+        #Genrate Some Error
+        form_data = form.find(pk)
+        return render(request,"update_form.html",{
+            "pk":pk,
+            "form":form_data
+        })
+    except Exception as e:
+        print(e)
+        #return 404
+        raise Http404("Form not Found")
